a4df76a62b76e08fe540c80ac73e45cb276bb20b
Vu Quoc Huy <huyvq.c633@gmail.com>
2018-06-21 20:26:15 +0200

    add blogtruyen.com

---
+++ bot/blogtruyen/blogtruyen.go
@@ -0,0 +1,58 @@
+package blogtruyen
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/c633/kindle-manga/bot"
+	"github.com/gocolly/colly"
+)
+
+func init() {
+	bot.Register(&collector{
+		Bot: &bot.Bot{},
+	})
+}
+
+type collector struct {
+	*bot.Bot
+}
+
+var _ bot.Collector = (*collector)(nil)
+
+func (b *collector) Page() string {
+	return "http://blogtruyen.com"
+}
+
+func (b *collector) GetLink(base string, chap int) string {
+	// should not use the base colly's collector
+	c := colly.NewCollector()
+	var link string
+	c.OnHTML(`#list-chapters p`, func(e *colly.HTMLElement) {
+		s := e.DOM.Children().First().Children().First()
+		href, _ := s.Attr("href")
+		title, _ := s.Attr("title")
+		if strings.Contains(title, strconv.Itoa(chap)) {
+			link = b.Page() + href
+		}
+	})
+
+	c.Visit(base)
+	return link
+}
+
+func (b *collector) Collect(base string, chap int, outputDir string) {
+	b.Colly = colly.NewCollector()
+
+	b.Colly.OnHTML(`#content`, func(e *colly.HTMLElement) {
+		e.ForEach("img", func(i int, ee *colly.HTMLElement) {
+			link := ee.Attr("src")
+			b.Colly.Visit(link)
+		})
+	})
+
+	b.Bot.Collect(base, chap, outputDir)
+
+	link := b.GetLink(base, chap)
+	b.Colly.Visit(link)
+}
--- import.go
+++ import.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "github.com/c633/kindle-manga/bot/blogtruyen"
 	_ "github.com/c633/kindle-manga/bot/hocvientruyentranh"
 	_ "github.com/c633/kindle-manga/bot/truyentranh"
 	_ "github.com/c633/kindle-manga/bot/truyentranhtuan"
