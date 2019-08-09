0d4fd1c2fdc1fc5a2bdc9f7ffa6fcc52c2b67fb5
Vu Quoc Huy <huyvq.c633@gmail.com>
2019-08-09 20:19:02 +0200

    add hamtruyentranh

---
+++ bot/hamtruyentranh/hamtruyentranh.go
@@ -0,0 +1,58 @@
+package hamtruyentranh
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/gocolly/colly"
+	"github.com/vqhuy/kindle-manga/bot"
+)
+
+func init() {
+	bot.Register(&collector{
+		Bot: &bot.Bot{},
+	})
+}
+
+type collector struct {
+	*bot.Bot
+}
+
+var _ bot.Collector = (*collector)(nil)
+
+func (b *collector) Page() string {
+	return "http://hamtruyentranh.com/"
+}
+
+func (b *collector) GetLink(base string, chap int) string {
+	// should not use the base colly's collector
+	c := colly.NewCollector()
+	var link string
+	c.OnHTML(`select.chapter-selected`, func(e *colly.HTMLElement) {
+		s := e.DOM.Children().First().Children().First()
+		href, _ := s.Attr("href")
+		title, _ := s.Attr("title")
+		if strings.Contains(title, strconv.Itoa(chap)) {
+			link = href
+		}
+	})
+
+	c.Visit(base)
+	return link
+}
+
+func (b *collector) Collect(base string, chap int, outputDir string) {
+	b.Colly = colly.NewCollector()
+
+	b.Colly.OnHTML(`div.manga-container`, func(e *colly.HTMLElement) {
+		e.ForEach("img", func(i int, ee *colly.HTMLElement) {
+			link := ee.Attr("src")
+			b.Visit(link)
+		})
+	})
+
+	b.Bot.Collect(base, chap, outputDir)
+
+	link := b.GetLink(base, chap)
+	b.Colly.Visit(link)
+}
--- import.go
+++ import.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "github.com/vqhuy/kindle-manga/bot/blogtruyen"
+	_ "github.com/vqhuy/kindle-manga/bot/hamtruyentranh"
 	_ "github.com/vqhuy/kindle-manga/bot/hocvientruyentranh"
 	_ "github.com/vqhuy/kindle-manga/bot/truyentranh"
 	_ "github.com/vqhuy/kindle-manga/bot/truyentranhtuan"
